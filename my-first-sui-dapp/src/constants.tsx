e7468ac1c91ac87b5e6f77a461a0c8b48e5972d8
J.A.R.V.I.S <aswina.n2005@gmail.com>
2025-09-05 20:02:38 +0530

    new package address and new feature added

--- my-first-sui-dapp/src/constants.tsx
+++ my-first-sui-dapp/src/constants.tsx
@@ -5,5 +5,5 @@
 
 // NFT Package IDs
 export const DEVNET_NFT_PACKAGE_ID = "0xTODO"; // Replace with your devnet package ID
-export const TESTNET_NFT_PACKAGE_ID = "0xebb2530005d81a3c6de4961d6d21d2c64bd94f53a4766dd70bfb8031b8e13e57";
+export const TESTNET_NFT_PACKAGE_ID = "0xfe371eba73638f083722abb05502621963fd225758c5c5c07568f59b0dbe547f";
 export const MAINNET_NFT_PACKAGE_ID = "0xTODO"; // Replace with your mainnet package ID
