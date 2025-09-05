c80510f3fc5138339a7bb6fd527014b8368184e2
J.A.R.V.I.S <aswina.n2005@gmail.com>
2025-09-05 17:59:08 +0530

    added gitignore

--- my-first-sui-dapp/src/constants.tsx
+++ my-first-sui-dapp/src/constants.tsx
@@ -5,5 +5,5 @@
 
 // NFT Package IDs
 export const DEVNET_NFT_PACKAGE_ID = "0xTODO"; // Replace with your devnet package ID
-export const TESTNET_NFT_PACKAGE_ID = "0xbe47df4fbec1d79b42bee2c42315b376f1c6401da77becf250ee4eed97d2f5c9";
+export const TESTNET_NFT_PACKAGE_ID = "0xebb2530005d81a3c6de4961d6d21d2c64bd94f53a4766dd70bfb8031b8e13e57";
 export const MAINNET_NFT_PACKAGE_ID = "0xTODO"; // Replace with your mainnet package ID
